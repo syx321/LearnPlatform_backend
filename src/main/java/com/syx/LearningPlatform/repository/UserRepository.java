e6b1b6b0e9b6056e788d5a86280ea5361d9926f8
ma.chunfei <ma.chunfei@hellogroup.com>
2023-05-17 18:32:36 +0800

    类型

--- src/main/java/com/syx/LearningPlatform/repository/UserRepository.java
+++ src/main/java/com/syx/LearningPlatform/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.syx.LearningPlatform.repository;
 
 import com.syx.LearningPlatform.model.User;
-import com.syx.LearningPlatform.model.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -14,21 +14,26 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 
     // 添加关注关系
+    @Transactional
     @Modifying
-    @Query("UPDATE User u SET u.following = :followedUser WHERE u.id = :userId")
-    void addFollowing(@Param("userId") Long userId, @Param("followedUser") User followedUser);
+    @Query(value = "insert into user_following(user_id,following_video_id) value (?1,?2)", nativeQuery = true)
+    void addFollowing(@Param("userId") Long userId, @Param("videoId") Long videoId);
 
     // 移除关注关系
+    @Transactional
     @Modifying
-    @Query("UPDATE User u SET u.following = :unfollowedUser WHERE u.id = :userId")
-    void removeFollowing(@Param("userId") Long userId, @Param("unfollowedUser") User unfollowedUser);
+    @Query(value = "delete from user_following where user_id =?1 and following_video_id=?2", nativeQuery = true)
+    void removeFollowing(@Param("userId") Long userId, @Param("videoId") Long videoId);
 
     // 添加收藏关系
+    @Transactional
     @Modifying
-    @Query("UPDATE User u SET u.favorites = :video WHERE u.id = :userId")
-    void addFavorite(@Param("userId") Long userId, @Param("video") Video video);
-   // 移除收藏关系
-   @Modifying
-   @Query("UPDATE User u SET u.favorites = :video WHERE u.id = :userId")
-   void removeFavorite(@Param("userId") Long userId, @Param("video") Video video);
+    @Query(value = "insert into user_favorites(user_id,favorites_video_id) value (?1,?2)", nativeQuery = true)
+    void addFavorite(@Param("userId") Long userId, @Param("videoId") Long videoId);
+
+    // 移除收藏关系
+    @Transactional
+    @Modifying
+    @Query(value = "delete from user_favorites where user_id =?1 and favorites_video_id=?2", nativeQuery = true)
+    void removeFavorite(@Param("userId") Long userId, @Param("videoId") Long videoId);
 }
