e225a1ee2950cca97c396bb09a90362efb57e791
ma.chunfei <ma.chunfei@hellogroup.com>
2023-05-17 14:00:23 +0800

    修改

--- src/main/java/com/syx/LearningPlatform/controller/UserVideoController.java
+++ src/main/java/com/syx/LearningPlatform/controller/UserVideoController.java
@@ -1,8 +1,15 @@
 package com.syx.LearningPlatform.controller;
 
+import com.syx.LearningPlatform.DTO.VideoDTO;
 import com.syx.LearningPlatform.service.UserVideoService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/users/{userId}/videos")
@@ -34,6 +41,12 @@ public class UserVideoController {
     @PostMapping("/{videoId}/unfavorite")
     public ResponseEntity<Void> unfavoriteVideo(@PathVariable("userId") Long userId, @PathVariable("videoId") Long videoId) {
         userVideoService.unfavoriteVideo(userId, videoId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<Void> upload(@PathVariable("userId") Long userId, @RequestParam("video") MultipartFile multipartFile, VideoDTO v) {
+        userVideoService.save(userId, multipartFile, v);
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/syx/LearningPlatform/service/UserVideoService.java
+++ src/main/java/com/syx/LearningPlatform/service/UserVideoService.java
@@ -1,5 +1,6 @@
 package com.syx.LearningPlatform.service;
 
+import com.syx.LearningPlatform.DTO.VideoDTO;
 import com.syx.LearningPlatform.model.User;
 import com.syx.LearningPlatform.model.UserVideo;
 import com.syx.LearningPlatform.model.UserVideoId;
@@ -7,13 +8,24 @@ import com.syx.LearningPlatform.model.Video;
 import com.syx.LearningPlatform.repository.UserRepository;
 import com.syx.LearningPlatform.repository.UserVideoRepository;
 import com.syx.LearningPlatform.repository.VideoRepository;
+import lombok.Cleanup;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.util.Optional;
 
 @Service
 public class UserVideoService {
     private final UserRepository userRepository;
     private final VideoRepository videoRepository;
     private final UserVideoRepository userVideoRepository;
+    @Value("${file.path}")
+    private String path;
 
     public UserVideoService(UserRepository userRepository, VideoRepository videoRepository, UserVideoRepository userVideoRepository) {
         this.userRepository = userRepository;
@@ -61,5 +73,30 @@ public class UserVideoService {
         UserVideo userVideo = userVideoRepository.findById(userVideoId).orElseThrow(() -> new IllegalArgumentException("UserVideo not found for user id: " + userId + " and video id: " + videoId));
 
         userVideoRepository.delete(userVideo);
+    }
+
+    @SneakyThrows
+    public void save(Long userId, MultipartFile multipartFile, VideoDTO v) {
+        if (multipartFile.isEmpty()) {
+            throw new IllegalArgumentException("not file");
+        }
+        String[] split = Optional.ofNullable(multipartFile.getOriginalFilename()).orElse("").split("\\.");
+        if (!"mp4".equals(split[1])) {
+            throw new IllegalArgumentException("file type error");
+        }
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("User not found with id: " + userId));
+
+        String filePath = path + split[0] + System.currentTimeMillis() + ".mp4";
+        @Cleanup FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+        @Cleanup BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        @Cleanup BufferedInputStream bufferedInputStream = new BufferedInputStream(multipartFile.getInputStream());
+        byte[] bytes = new byte[2048];
+        int len;
+        while ((len = bufferedInputStream.read(bytes)) != -1) {
+            bufferedOutputStream.write(bytes, 0, len);
+        }
+        Video video = videoRepository.save(new Video(v.getTitle(), v.getDescription(), filePath));
+        UserVideoId userVideoId = new UserVideoId(user.getId(), video.getId());
+        userVideoRepository.save(new UserVideo(userVideoId, user, video));
     }
 }
